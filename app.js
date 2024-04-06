23827c74c42114b5aafc2295e4d80b3b792894ce
Prasoon-Lohani <132545785+Prasoon-Lohani@users.noreply.github.com>
2024-04-06 13:12:05 +0530

    build: cors integration

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const bodyParser = require('body-parser');
-const express  = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+var corsOption = {
+  origin: "*",
+  methods: "GET,POST",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOption));
 
-const routes = require('./routes/routes');
-app.use('/api/v1',routes);
+const routes = require("./routes/routes");
+app.use("/api/v1", routes);
 
 module.exports = app;
