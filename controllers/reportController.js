e8d4b05b8a003e66aee2d723ee2dc93f28d6ae90
Prasoon-Lohani <132545785+Prasoon-Lohani@users.noreply.github.com>
2024-04-06 14:18:36 +0530

    update

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,11 +1,13 @@
 const URL = require("../model/urldb");
 
 const reportURL = async (req, res, next) => {
+  console.log("Report Route");
   const { url } = req.body;
-
+  console.log('URL: ',url);
   try {
     if (url) {
-      const url = await URL.create({ url, safe: false });
+      const url = URL({ url, safe: false });
+      await URL.save();
       return res.status(201).json({ ack: true, msg: "Reported Successfully" });
     } else return res.status(404).json({ ack: false, err: "No URL provided" });
   } catch (err) {
